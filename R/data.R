70af57016c60ddb05f9ec69ce7700ff61bb85ec0
Francesco Bailo <me@francescobailo.net>
2019-01-30 15:14:41 +1100

    Add

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Polygons of the Mexican state of Aguascalientes (2010).
+#'
+#' Simple Features containing randomly generated population and votes variables.
+#' diamonds.
+#'
+#' @format 589 simple features with corresponding data:
+#' \describe{
+#'   \item{gid}{unique id}
+#'   \item{geometry}{geometry of the feature}
+#'   \item{population}{randomly generated}
+#'   \item{party_a_votes}{randomly generated}
+#'   \item{party_b_votes}{randomly generated}
+#' }
+#' @source \url{http://gaia.inegi.org.mx/}
+"poly_a"
+
+#' Polygons of the Mexican state of Aguascalientes (2017).
+#'
+#' Simple Features containing randomly generated population and votes variables.
+#' diamonds.
+#'
+#' @format 602 simple features with corresponding data:
+#' \describe{
+#'   \item{gid}{unique id}
+#'   \item{geometry}{geometry of the feature}
+#'   \item{population}{randomly generated}
+#'   \item{party_a_votes}{randomly generated}
+#'   \item{party_b_votes}{randomly generated}
+#' }
+#' @source \url{http://gaia.inegi.org.mx/}
+"poly_b"
